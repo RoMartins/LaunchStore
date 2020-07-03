32cc054667f17d97cf18980613ed9b5891fc38f9
Rodrigo Martins <rodrigomartins_santos1804@hotmail.com>
2020-07-02 23:53:41 -0300

    Service commit

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,32 +1,16 @@
-const {FormatPrice} = require('../../lib/functions')
 
-const Category = require("../models/Category")
 const ProductModel = require("../models/productModel")
-const File = require("../models/FileModel")
+const LoadProductsService = require("../services/LoadProductService")
 
 module.exports = {
     async index(req, res){
-        const products =await ProductModel.FindAll()
+        let products =await ProductModel.FindAll()
 
         if(!products) return res.send("Produto não encontrado")
     
-        async function getImage(product) {
-            let files = await ProductModel.files(product.id)
-             files = files.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
-            
-           const File = files[0].replace(/\\/g, '/')
-            return File
-        }
+      
+         products = await LoadProductsService.load('products')
 
-        const productsPromise = products.map(async product => {
-            product.img = await getImage(product)
-            product.price = FormatPrice(product.price)
-            product.oldPrice = FormatPrice(product.old_price)
-            
-            return product
-        })
-        const lastAdded = await Promise.all(productsPromise)
-
-        return res.render("home/index", {products : lastAdded})
+        return res.render("home/index", {products})
     },
 }
---
+++ src/app/services/LoadProductService.js
@@ -0,0 +1,57 @@
+
+const ProductModel = require("../models/productModel")
+const {FormatPrice, date} = require('../../lib/functions')
+
+async function getImages(productId) {
+    let files = await ProductModel.files(productId)
+     files = files.map(file =>({
+         ...file,
+         src: `${file.path.replace("public", "")}`
+     }))
+        return files
+ 
+        //const File = files[0]
+}
+
+async function format(product){
+        const files =  await getImages(product.id)
+        product.img = files[0].src.replace(/\\/g, '/')
+        product.FormatPrice = FormatPrice(product.price)
+        product.FormatOldPrice = FormatPrice(product.old_price)
+
+        const { day, hour, minutes, mounth} = date(product.updated_at)
+        
+        product.published = {
+            day :`${day}/${mounth}`,
+            hour: `${hour}h${minutes}`
+        }
+        
+        return product
+}
+
+const LoadService = {
+    load(service, filter){
+        this.filter = filter
+        return this[service]()
+    },
+   async product(){
+        try {
+        const product =  await ProductModel.FindOne(this.filter)
+        
+        return format(product)
+        
+        } catch (error) {
+            console.error(error);
+            
+        }
+    },
+    async products(){
+        const products = await ProductModel.FindAll(this.filter)
+        
+        const productsPromise = products.map(format)
+        return Promise.all(productsPromise)
+    },
+    format,
+}
+
+module.exports = LoadService
