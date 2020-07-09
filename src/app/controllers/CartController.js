7f15dec334f751aafc8fe4f3998f205a73c75a9d
Rodrigo Martins <rodrigomartins_santos1804@hotmail.com>
2020-07-08 23:01:44 -0300

    CART commit

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -3,10 +3,61 @@ const LoadProduct = require("../services/LoadProductService")
 module.exports = {
     async index(req, res){
      let {cart} = req.session
-    const product = await LoadProduct.load('product', {where: {id: 26}})
-    cart = Cart.init(cart).addOne(product)
+     
+     cart = Cart.init(cart)
       
 
         return res.render("cart/index", {cart})
+    },
+    async addOne(req, res){
+        //pegar o id do produto e o produto
+        const {id} = req.params
+        const product = await LoadProduct.load('product', {where: {id}})
+
+        //pegar o carrinho da sessão 
+        let {cart} = req.session
+
+        //add o produto no carrinho
+        cart = Cart.init(cart).addOne(product)
+
+        //atualizar o carrinho 
+        req.session.cart = cart
+        
+        // redirecionar o usuario
+        return res.redirect('/cart')
+    },
+    removeOne(req, res){
+         //pegar o id do produto
+         const {id} = req.params
+        // pegar carrinho da sessão
+        let {cart} = req.session
+
+        //se não tiver carrinho 
+        if(!cart) return 
+        //iniciar o carrinho
+        cart = Cart.init(cart).removeOne(id)
+        //atualizar o carrinho, removendo 1 item.
+        req.session.cart = cart
+
+        //redirect
+        return res.redirect('/cart')
+
+    },
+    delete(req,res) {
+        //pegar o id do produto
+         const {id} = req.params
+         // pegar carrinho da sessão
+         let {cart} = req.session
+ 
+         //se não tiver carrinho 
+         if(!cart) return 
+           //iniciar o carrinho
+        cart = Cart.init(cart).delete(id)
+        //atualizar o carrinho, removendo 1 item.
+        req.session.cart = cart
+
+        //redirect
+        return res.redirect('/cart')
+
     },
 }
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -3,6 +3,9 @@ const routes = express.Router()
 const CartController = require('../app/controllers/CartController')
 
 routes.get('/', CartController.index)
+routes.post('/:id/add', CartController.addOne)
+routes.post('/:id/remove', CartController.removeOne)
+routes.post('/:id/delete', CartController.delete)
 
 
 
